03d4b09686b613d52ad6cfc5e2a4eb286beb4b79
viciscat <51047087+viciscat@users.noreply.github.com>
2025-09-20 21:30:02 +0200

    better names

--- SuperHexagonMod/dllmain.cpp
+++ SuperHexagonMod/dllmain.cpp
@@ -2,25 +2,31 @@
 #include <safetyhook.hpp>
 using PrintFunc_t = void(__thiscall*)(void* /*this*/, int x, int y, std::basic_string<char> param_3, int r, int g, int b, bool centered);
 
-PrintFunc_t graphics_printString = nullptr;
-
 enum
 {
     IMAGE_BASE = 0x00400000
 };
 
 HMODULE targetModule;
-uintptr_t GetAddress(uintptr_t ghidraAddress)
+uintptr_t get_address(uintptr_t ghidraAddress)
 {
     uintptr_t moduleBase = reinterpret_cast<uintptr_t>(targetModule);
     return moduleBase + (ghidraAddress - IMAGE_BASE);
 }
-void initialize_game_functions()
+
+namespace
 {
-    graphics_printString = reinterpret_cast<PrintFunc_t>(GetAddress(0x00422c90));
+    PrintFunc_t graphics_printString = nullptr;
+    void initialize_game_functions()
+    {
+        graphics_printString = reinterpret_cast<PrintFunc_t>(get_address(0x00422c90));
+    }
 }
 
-void test_hook(SafetyHookContext& ctx)
+
+
+
+void hook_patch_text(SafetyHookContext& ctx)
 {
     void* thisPtr = reinterpret_cast<void*>(ctx.edi);
     graphics_printString(thisPtr, 0, 5, "FPS Unlock Patch", 255, 0, 0, false);
@@ -32,15 +38,15 @@ DWORD WINAPI MainThread(LPVOID lpParam)
 {
     targetModule = GetModuleHandle(L"SuperHexagon.exe");
     initialize_game_functions();
-    uintptr_t uintptr = GetAddress(0x00407c1d);
-    hook = safetyhook::create_mid(uintptr, test_hook);
+    uintptr_t uintptr = get_address(0x00407c1d);
+    hook = safetyhook::create_mid(uintptr, hook_patch_text);
     //char buffer[256];
     //sprintf_s(buffer, "Test %08x %08x\n", targetModule);
     //MessageBoxA(nullptr, buffer, buffer, MB_OK);
 
     HANDLE current_process = GetCurrentProcess();
     constexpr byte data[] = {120};
-    WriteProcessMemory(current_process, reinterpret_cast<LPVOID>(GetAddress(0x432640)), data, 1, nullptr); // NOP out a jump to skip our hook
+    WriteProcessMemory(current_process, reinterpret_cast<LPVOID>(get_address(0x432640)), data, 1, nullptr); // NOP out a jump to skip our hook
     
     while (true)
     {
